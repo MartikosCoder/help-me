784ef7581cf6d654283e9386d9145f29d0057695
Martin <martbelmoaw@gmail.com>
2018-09-23 12:45:42 +0300

    working

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
 module.exports = function count(s, pairs) {
-  let result = 0;
-  let N = 1;
-  s = s.split('');
-  const gcd = (x, y) => {
-    x = Math.abs(x);
-    y = Math.abs(y);
-    while(y) {
-      var t = y;
-      y = x % y;
-      x = t;
-    }
-    return x;
+  if (pairs.length>=8){
+    return 0;
   }
-
-  for (let i = 0; i < pairs.length; i++){
-    N += (pairs[i][0] ** (pairs[i][1]));
-  }
-
-  console.log("k j gcd res");
-  
-  for (let k = 1; k <= N; k++){
-    for (let j = 0; j < s.length; j++){
-      if(s[j] == 1 && k + j !== N){
-        if(gcd(k + j, N) === 1){
-          console.log(k + ' ' + j + ' ' + gcd(k + j, N) + ' ' + ++result);
-        }
-
-        /*if(N / k + j === N || !(N / k + j).toString().includes('.5') && (N / k + j).toString().includes('.')) {
-          
-        }*/
-      } else if(s[j] == 0){
-        if(gcd(k + j, N) !== 1){
-          console.log(k + ' ' + j + ' ' + gcd(k + j, N) + ' ' + ++result);
-        }
-
-        /*if(!(N / k + j === N || !(N / k + j).toString().includes('.5') && (N / k + j).toString().includes('.'))) {
-        
-        }*/
+    let N=1;
+    let min=pairs[0];
+    for (let i=0;i<pairs.length;i++){
+      N*=pairs[i][0];
+      if (pairs[i][1]<min[1]){
+        min=pairs[i];
       }
     }
+    let counter=0;
+    let tmp;
+    for (let i =1;i<=N;i++){
+      tmp=0;
+      for (let j=0;j<s.length;j++){
+        if (s[j]==1){
+          for (let z=0;z<pairs.length;z++){
+            if ((i+j)%pairs[z][0]!=0){
+              tmp=1;		
+            } else {
+              tmp=0;
+              break;
+            }	
+          }
+        }else{
+          for (let z=0;z<pairs.length;z++){
+            if ((i+j)%pairs[z][0]==0){
+              tmp=1;
+              break;
+            } else {
+              tmp=0;	
+            }
+          }				
+        }	
+          if (tmp==0){
+            break;
+          }
+      }
+        if (tmp==1){
+          counter++;
+        }
+    }
+        
+    if (min[1]%8==0){
+      counter*=Math.pow(min[0],7);
+      min[1]/=8;
+      N*=Math.pow(pairs[0][0],7);
+    }
+    for	(let i=1;i<min[1];i++){
+        counter*=N;
+        counter=counter%1000000007;
+      }
+    for (let j=0;j<pairs.length;j++){
+      for	(let i=1;i<=(pairs[j][1]-min[1]);i++){
+        counter*=pairs[j][0];
+        counter=counter%1000000007;
+      }	
+    } 
+    return counter;
   }
-
-  return result % 1000000007;
-}
